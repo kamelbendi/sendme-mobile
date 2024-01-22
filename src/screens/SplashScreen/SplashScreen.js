0ab82f8ae9a93f6f53e57b9e92e69bb8cb5b13a8
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-23 00:08:57 +0100

    SplashScreen set up

---
+++ src/screens/SplashScreen/SplashScreen.js
@@ -0,0 +1,26 @@
+// SplashScreen.js
+import React from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+import Text from '../../components/Text';
+import { useMainContext } from '../../store/MainContext';
+
+const SplashScreen = () => {
+  const { mainState, setMainState } = useMainContext();
+
+  return (
+    <View style={styles.container}>
+      <Text>{mainState.alignItems.sendme}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  }
+});
+
+export default SplashScreen;
