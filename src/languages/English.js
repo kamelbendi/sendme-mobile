19a6084b93e7a6a4ee800c7649b3384ba14547fa
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-22 22:41:13 +0100

    language setup

---
+++ src/languages/English.js
@@ -0,0 +1,18 @@
+const english = {
+    email: 'email',
+    password: 'password',
+    signIn: 'Sign In',
+    signUp: 'Sign Up',
+    repeatPassword: 'Repeat password',
+    firstName: 'First name',
+    lastName: 'Last name',
+    fillEmail: 'Please provide your email',
+    fillPassword: 'Please provide your password',
+    registrationText: 'fill out the form and complete the registration',
+    loginText: 'fill out the form to log in',
+    registerOrLogin: 'Please Sign in or create a new account',
+    welcomeText: 'Welcome to SendMe app!',
+
+  };
+
+  export default english;
---
+++ src/languages/French.js
@@ -0,0 +1,18 @@
+const french = {
+    email: 'email',
+    password: 'Mot de passe',
+    signIn: 'Se connecter',
+    signUp: 'S\'enregistrer',
+    repeatPassword: 'Repeter le mot de passe',
+    firstName: 'Prenom',
+    lastName: 'Nom',
+    fillEmail: 'Veillez completer votre email',
+    fillPassword: 'Veiller completer votre mot de passe',
+    registrationText: 'Completez le formulair pour s\'enregistrer',
+    loginText: 'Completez le formulaire pour se connecter',
+    registerOrLogin: 'Veillez vous connecter ou bien creer votre compte',
+    welcomeText: 'Bienvenue a SendMe app!',
+
+  };
+  
+  export default french;
