334badcede462656a69fca057e8c79a03d93d025
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-23 03:46:23 +0100

    reg ex email validation logic

---
+++ src/helpers/EmailValidation.js
@@ -0,0 +1,6 @@
+export default function emailValidation(email) {
+    const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  
+    return Boolean(email && regex.test(email));
+  }
+  
