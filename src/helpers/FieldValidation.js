d795509ac75a6641281971167d861094f817967d
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-23 03:46:41 +0100

    reg ex field validation logic

---
+++ src/helpers/FieldValidation.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function fieldValidation(name) {
+    const regex = /^[a-zA-Z\s]+$/i;
+  
+    return Boolean(name && regex.test(name) && name.length <= 120);
+  }
+  
