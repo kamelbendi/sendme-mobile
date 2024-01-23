31d7b77917ffc6cb04d6eb01189d21c75aa4bc93
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-23 03:47:39 +0100

    reg ex strong password validation logic

---
+++ src/helpers/PasswordValidation.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function passwordValidation(password) {
+    const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[~!@#%&_\+\{\}":\?\>\<\[\];'\/\.\\-]).{8,}$/i;
+  
+    if (password.length < 8) {
+      return false;
+    }
+    return regex.test(password);
+  }
+  
