7a0a988719cd252ec8ada18b4256e918290d3bb0
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-24 21:04:11 +0100

    url update

--- api-urls.js
+++ api-urls.js
@@ -1,6 +1,9 @@
 const apiUrl = {
-    login: '',
+    login: 'http://localhost:3030/login',
+    register: 'http://localhost:3030/register',
+    verifySMS: 'http://localhost:3030/verifysms',
+    getSMS: 'http://localhost:3030/getsms',
   };
   
-  export default apiUrl;
-  
+export default apiUrl;
+ 
