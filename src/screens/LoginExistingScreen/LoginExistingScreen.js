47e64459d277e41fd3fc4ddf97e1776b729b6ce5
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-10 14:06:50 +0100

    loging to existing user final

--- src/screens/LoginExistingScreen/LoginExistingScreen.js
+++ src/screens/LoginExistingScreen/LoginExistingScreen.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import Text from '../../components/Text';
-//import * as styles from './LoginExistingScreen.styles';
-//import { LoginExistingScreenWrapper } from './LoginExistingScreen.styles';
+import PinComponent from '../../components/PinComponent';
+import Loader from '../../components/Loader';
+import { useMainContext } from '../../store/MainContext';
+import axios from 'axios';
+import apiUrl from '../../../api-urls';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LOCAL_STORAGE_NAME } from '../../store/user-details';
 
-const LoginExistingScreen = (props) => (
-  <View >
-    <Text>Test content</Text>
-  </View>
-);
+const LoginExistingScreen = (props) => {
+  const [loading, setLoading] = useState(false);
+  const { mainState, setMainState } = useMainContext();
+  const setUpPin = async (pin) => {
+    setLoading(true);
+    const email = await AsyncStorage.getItem(LOCAL_STORAGE_NAME);
+
+    emailToUse = JSON.parse(email);
+    if (emailToUse && typeof emailToUse === 'string') {
+      console.log(emailToUse, pin)
+      await axios.post(apiUrl.pinlogin, {
+        email: emailToUse,
+        pin: pin
+      })
+      .then(res => {
+        console.log('API Response:', res.data);
+        setLoading(false);
+        setMainState({...mainState, userDetails: {...mainState.userDetails, token: res.data.token}});
+        props.navigation.replace('Dashboard');
+      })
+      .catch(error => {
+        console.error('API Error:', error);
+        setLoading(false);
+        alert('Incorrect pin or API error');
+      });
+    }
+  };
+
+  return (
+    <>
+      {loading && <Loader loading={loading} />}
+      <PinComponent
+        setUpPin={setUpPin}
+        title={mainState.language.sendme}
+        underInputText={mainState.language.loginWithYourPin}
+      />
+    </>
+)
+};
 
 LoginExistingScreen.propTypes = {
   // bla: PropTypes.string,
