87da8454ac2fec72225dd9e53caf8ebdd3f78229
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-10 23:33:55 +0100

    (logic) name and surname valid, username unique and valid

--- src/screens/RegisterScreen/RegisterScreen.js
+++ src/screens/RegisterScreen/RegisterScreen.js
@@ -1,6 +1,6 @@
 
 // Import React and Component
-import React, {useState, createRef} from 'react';
+import React, {useState, createRef, useEffect} from 'react';
 import {
   StyleSheet,
   TextInput,
@@ -20,16 +20,21 @@ import apiUrl from '../../../api-urls';
 import { useMainContext } from '../../store/MainContext';
 import XButton from '../../components/XButton';
 import emailValidation from '../../helpers/EmailValidation';
+import fieldValidation from '../../helpers/FieldValidation';
+import usernameValidation from '../../helpers/UserNameValidation';
+import axios from 'axios';
 
 const RegisterScreen = (props) => {
   const { mainState, setMainState } = useMainContext();
   const [loading, setLoading] = useState(false);
   const [form, setForm] = useState({
     isEmailDirty: false,
+    isEmailUnique: false,
     isPasswordDirty: false,
     isNameDirty: false,
     isSurnameDirty: false,
     isUserNameDirty: false,
+    isUsernameUnique: false,
 
     name: '',
     surname: '',
@@ -38,14 +43,24 @@ const RegisterScreen = (props) => {
     password: '',
     confirmPassword: '',
   });
-  const [errortext, setErrortext] = useState('');
+  const [errorSurnameText, setErrorSurnameText] = useState('');
+  const [errorNameText, setErrorNameText] = useState('');
+  const [errorUsernameText, setErrorUsernameText] = useState('');
+  const [errorUsernameDUnique, setErrorUsernameUnique] = useState('');
+  const [errorEmailText, setErrorEmailText] = useState('');
+  const [errorEmailUnique, setErrorEmailUnique] = useState('');
+  const [errorPasswordText, setErrorPasswordText] = useState('');
+  const [errorConfirmPasswordText, setErrorConfirmPasswordText] = useState('');
 
+  const nameInputRef = createRef();
+  const surnameInputRef = createRef();
+  const usernameInputRef = createRef();
   const emailInputRef = createRef();
   const passwordInputRef = createRef();
   const confirmPasswordInputRef = createRef();
 
-  const handleSubmitButton = () => {
-    setErrortext('');
+  const handleSubmitButton = async () => {
+    setErrorNameText('');
     if (!form.name) {
       alert('Please fill Name');
       return;
@@ -90,18 +105,77 @@ const RegisterScreen = (props) => {
         password: form.password
       }
     });
+
     props.navigation.navigate('PhoneRequestScreen');
     setLoading(false);
   };
 
   const goToWelcomeScreen = () => {
-    console.log('heee')
     props.navigation.navigate('WelcomeScreen');
   }
 
+  const handleSurnameInput = (surname) => {
+    setForm({...form, surname, isSurnameDirty: fieldValidation(surname)});
+    
+    if (!fieldValidation(surname)) {
+      setErrorSurnameText('Not a valid surname')
+    } else {
+      setErrorSurnameText('')
+    }
+  }
+
+  const handleNameInput = (name) => {
+    setForm({...form, name, isNameDirty: fieldValidation(name)});
+    
+    if (!fieldValidation(name)) {
+      setErrorNameText('Not a valid Name')
+    } else {
+      setErrorNameText('')
+    }
+  }
+
+  const handleUsernameInput = async (username) => {
+
+    setForm({...form, username, isUserNameDirty: usernameValidation(username)});
+    
+    if (!usernameValidation(username)) {
+      setErrorUsernameText('Not a valid Username');
+      setErrorUsernameUnique('');
+      return;
+    }
+    
+    setErrorUsernameText('');
+    const isUsernameUnique = await isUsernameUniqueFunc(username);
+    setForm({...form, isUsernameUnique: isUsernameUnique})
+    if (!isUsernameUnique) {
+      setErrorUsernameUnique('Username is not unique, choose a different one');
+    } else {
+      setErrorUsernameUnique('Username is unique');
+    }
+  }
+
+  const isUsernameUniqueFunc = async (username) => {
+    try {
+      const response = await axios.post(apiUrl.checkUsername, { username: username });
+      console.log(response.data.unique)
+      return response.data.unique; // This will be true if the username is unique, false otherwise
+    } catch (error) {
+      console.log(response.data.unique)
+      console.error('Error checking username:', error);
+      // Handle the error appropriately
+      return false; // Assuming non-unique in case of an error
+    }
+  }
+
+  useEffect(() => {
+    surnameInputRef.current && surnameInputRef.current.focus();
+  }, [])
+
   return (
     <View style={{flex: 1, backgroundColor: '#1e1e1e'}}>
-      <XButton onPress={goToWelcomeScreen}></XButton>
+      <TouchableOpacity onPress={goToWelcomeScreen} style={styles.closeButton} activeOpacity={0.5}>
+        <Text style={styles.closeButtonText}>X</Text>
+      </TouchableOpacity>
       <Loader loading={loading} />
       <ScrollView
         keyboardShouldPersistTaps="handled"
@@ -116,52 +190,76 @@ const RegisterScreen = (props) => {
           <View style={styles.SectionStyle}>
             <TextInput
               style={styles.inputStyle}
-              onChangeText={(surname) => setForm({...form, surname})}
+              onChangeText={(surname) => handleSurnameInput(surname)}
+              ref={surnameInputRef}
               underlineColorAndroid="#f000"
               placeholder={mainState.language.enterSurname}
               placeholderTextColor="#8b9cb5"
               autoCapitalize="sentences"
               returnKeyType="next"
               onSubmitEditing={() =>
-                emailInputRef.current && emailInputRef.current.focus()
+                nameInputRef.current && nameInputRef.current.focus()
               }
               blurOnSubmit={false}
             />
           </View>
+            {errorSurnameText != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errorSurnameText}
+            </Text>
+          ) : null}
           <View style={styles.SectionStyle}>
             <TextInput
               style={styles.inputStyle}
-              onChangeText={(name) => setForm({...form, name})}
+              onChangeText={(name) => handleNameInput(name)}
+              ref={nameInputRef}
               underlineColorAndroid="#f000"
               placeholder={mainState.language.enterName}
               placeholderTextColor="#8b9cb5"
               autoCapitalize="sentences"
               returnKeyType="next"
               onSubmitEditing={() =>
-                emailInputRef.current && emailInputRef.current.focus()
+                usernameInputRef.current && usernameInputRef.current.focus()
               }
               blurOnSubmit={false}
             />
           </View>
+            {errorNameText != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errorNameText}
+            </Text>
+          ) : null}
           <View style={styles.SectionStyle}>
             <TextInput
               style={styles.inputStyle}
-              onChangeText={(username) => setForm({...form, username})}
-              underlineColorAndroid="#f000"
+              onChangeText={async (username) => handleUsernameInput(username)}
+              ref={usernameInputRef}
               placeholder={mainState.language.enterUsername}
               placeholderTextColor="#8b9cb5"
               returnKeyType="next"
+              autoCapitalize="none"
               onSubmitEditing={() =>
                 emailInputRef.current && emailInputRef.current.focus()
               }
               blurOnSubmit={false}
             />
           </View>
+            {errorUsernameText != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errorUsernameText}
+            </Text>
+          ) : null}
+            {errorUsernameDUnique != '' ? (
+            <Text style={form.isUsernameUnique ? styles.successTextStyle : styles.errorTextStyle}>
+              {errorUsernameDUnique}
+            </Text>
+          ) : null}
           <View style={styles.SectionStyle}>
             <TextInput
               style={styles.inputStyle}
               onChangeText={(email) => setForm({...form, email})}
               underlineColorAndroid="#f000"
+              autoCapitalize="none"
               placeholder={mainState.language.enterEmail}
               placeholderTextColor="#8b9cb5"
               keyboardType="email-address"
@@ -174,6 +272,11 @@ const RegisterScreen = (props) => {
               blurOnSubmit={false}
             />
           </View>
+            {errorEmailText != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errorEmailText}
+            </Text>
+          ) : null}
           <View style={styles.SectionStyle}>
             <TextInput
               style={styles.inputStyle}
@@ -181,13 +284,23 @@ const RegisterScreen = (props) => {
               }
               underlineColorAndroid="#f000"
               placeholder={mainState.language.enterPassword}
+              autoCapitalize="none"
               placeholderTextColor="#8b9cb5"
               ref={passwordInputRef}
               returnKeyType="next"
               secureTextEntry={true}
               blurOnSubmit={false}
+              onSubmitEditing={() =>
+                confirmPasswordInputRef.current &&
+                confirmPasswordInputRef.current.focus()
+              }
             />
           </View>
+            {errorPasswordText != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errorPasswordText}
+            </Text>
+          ) : null}
           <View style={styles.SectionStyle}>
             <TextInput
               style={styles.inputStyle}
@@ -195,15 +308,16 @@ const RegisterScreen = (props) => {
               underlineColorAndroid="#f000"
               placeholder={mainState.language.confirmPassword}
               placeholderTextColor="#8b9cb5"
-              ref={passwordInputRef}
+              ref={confirmPasswordInputRef}
+              autoCapitalize="none"
               returnKeyType="next"
               secureTextEntry={true}
               blurOnSubmit={false}
             />
           </View>
-          {errortext != '' ? (
+            {errorConfirmPasswordText != '' ? (
             <Text style={styles.errorTextStyle}>
-              {errortext}
+              {errorConfirmPasswordText}
             </Text>
           ) : null}
           <TouchableOpacity
@@ -272,9 +386,23 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   successTextStyle: {
-    color: 'white',
+    color: 'green',
     textAlign: 'center',
-    fontSize: 18,
-    padding: 30,
+    fontSize: 14,
+  },
+  closeButton: {
+    position: 'relative',
+    backgroundColor: 'transparent',
+    marginTop: 90,
+    marginLeft: 20,
+    flexDirection: 'column',
+  },
+  closeButtonText: {
+    color: 'white', // Customize the text color
+    fontSize: 28,
+    fontWeight: 'bold',
+    position: 'absolute',
+    bottom: 0,
+    left: 10,
   },
 });
