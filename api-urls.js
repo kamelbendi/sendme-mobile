bf0d16ba391751e710368b7e52423d38c9f4ae18
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-10 14:05:30 +0100

    login, pin login final working version

--- api-urls.js
+++ api-urls.js
@@ -1,9 +1,13 @@
 const apiUrl = {
-    login: 'http://localhost:3030/login',
-    register: 'http://localhost:3030/register',
-    verifySMS: 'http://localhost:3030/verifysms',
-    getSMS: 'http://localhost:3030/getsms',
-  };
-  
+    login: 'http://localhost:3000/login',
+    pinlogin: 'http://localhost:3000/pinlogin',
+    user: 'http://localhost:3000/user',
+    register: 'http://localhost:3000/register',
+    verifySMS: 'http://localhost:3000/verifysms',
+    getSMS: 'http://localhost:3000/getsms',
+    checkUsername: 'http://localhost:3000/check-username',
+    checkEmail: 'http://localhost:3000/check-email',
+
+};
+
 export default apiUrl;
- 
