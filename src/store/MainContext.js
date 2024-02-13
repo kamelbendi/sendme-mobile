87755f3db73afe157803701d26ae227d48d248ab
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-13 21:44:59 +0100

    bugfixes: (login wrong account)

--- src/store/MainContext.js
+++ src/store/MainContext.js
@@ -1,11 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getCookie } from '../helpers/UserSignedIn';
 import english from '../languages/English.js';
-import { LOCAL_STORAGE_NAME } from './user-details';
-
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import apiUrl from '../../api-urls.js';
-import axios from 'axios';
 
 const MainContext = React.createContext();
 
@@ -15,61 +10,18 @@ export const initialMainState = {
     userExists: false, // Initialize with false
     userDetails: {
       pin: '',
-      //address: '',
-      //zipCode: '',
-      //country: '',
-      //city: '',
       email: '',
       name: '',
       surname: '',
       username: '',
       phone: '',
       token: '',
+      idUri: null
     },
 }
 
 export const MainContextProvider = ({ children }) => {
   const [mainState, setMainState] = useState(initialMainState);
-
-  const fetchUserDetails = async () => {
-    try {
-      //await AsyncStorage.clear();
-      const userDetailsString = await AsyncStorage.getItem(LOCAL_STORAGE_NAME);
-      
-      // Check if userDetailsString is a valid JSON string
-      if (userDetailsString) {
-        const parsedUserDetails = JSON.parse(userDetailsString);
-        
-        // Check if parsedUserDetails is an object
-        if (parsedUserDetails && typeof parsedUserDetails === 'string') {
-          axios.post(apiUrl.user, {
-            email: parsedUserDetails
-          })
-            .then(res => {
-              setMainState((prev) => ({
-                ...prev,
-                userExists: true,
-                userDetails: {
-                  name: res.data.name,
-                  surname: res.data.surname,
-                  username: res.data.username,
-                  phone: res.data.phone
-                },
-              }));
-            })
-            .catch(err => {
-              alert(err);
-            })
-        }
-      }
-    } catch (error) {
-      console.error('Error fetching user details:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUserDetails();
-  }, []);
 
   return (
     <MainContext.Provider value={{ mainState, setMainState }}>
