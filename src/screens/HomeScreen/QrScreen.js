bba4390256eded7a7ca6ee9d0fc0e5dc5e6f5dc2
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-20 09:42:33 +0100

    QR screen final

---
+++ src/screens/HomeScreen/QrScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+import { useMainContext } from '../../store/MainContext';
+import Text from '../../components/Text';
+
+const QrScreen = (props) => {
+  const { mainState, setMainState } = useMainContext();
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1e1e1e' }}>
+        <Text heavy large style={{ marginBottom: 40 }}>Username: {mainState.userDetails.username}</Text>
+        <QRCode
+          value={mainState.userDetails.username}
+          size={200}
+          color="white"
+          backgroundColor="#1e1e1e"
+        />
+      </View>
+    
+
+  )
+};
+
+export default QrScreen;
