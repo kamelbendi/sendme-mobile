e6643073bca2f4112a3f58112d54fe92be218ee2
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-21 09:49:15 +0100

    QRCodeScannerScreen final

--- src/components/QRCodeScanner.js
+++ src/components/QRCodeScanner.js
@@ -1,23 +1,78 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import QRCodeScanner from 'react-native-qrcode-scanner';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { CameraView, Camera } from "expo-camera/next";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Text from './Text';
 
-const QRCodeScannerComponent = () => {
-  const onRead = (e) => {
-    // Handle the QR code data
-    console.log('QR Code Data:', e.data);
+
+const QRCodeScannerScreen = (props) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const { onQRCodeScan } = props.route.params;
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    onQRCodeScan(data);
   };
 
+  if (hasPermission === null) {
+    return <Text style={{color: 'white', fontSize: 50}}>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text style={{color: 'white', fontSize: 50}}>No access to camera</Text>;
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <QRCodeScanner
-        onRead={onRead}
-        reactivate={true}
-        reactivateTimeout={2000}
-        showMarker={true}
+    <View style={styles.container}>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{
+          barCodeTypes: ["qr", "pdf417"],
+        }}
+        style={StyleSheet.absoluteFillObject}
       />
+      {scanned && (
+        <TouchableOpacity onPress={() => setScanned(false)} >
+          <Text>Tap to Scan Again</Text>
+        </TouchableOpacity>
+      )}
+        <TouchableOpacity style={styles.closeButton} onPress={() => props.navigation.navigate('Dashboard')}>
+          <Text style={styles.closeButtonText} >X</Text>
+        </TouchableOpacity>
     </View>
   );
-};
+}
 
-export default QRCodeScannerComponent;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  closeButton: {
+    backgroundColor: 'transparent',
+    height: 60,
+    width: 60,
+    marginTop: 80,
+    marginLeft: 40,
+    border: 10,
+    borderColor: 'white',
+    
+  },
+  closeButtonText: {
+    color: 'grey',
+    fontSize: 50,
+    fontWeight: 'bold',
+    position: 'absolute',
+    bottom: 0,
+    left: 10,
+  },
+});
+export default QRCodeScannerScreen;
